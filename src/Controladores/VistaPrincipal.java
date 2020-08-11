7a2b9a927717b4008bfd7f30b29a81b235cc9caa
Ariel <arxiel29@gmail.com>
2020-08-11 13:39:30 -0400

    Agregar logos a Vista Principal

--- src/Controladores/VistaPrincipal.java
+++ src/Controladores/VistaPrincipal.java
@@ -1,7 +1,16 @@
 package Controladores;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class VistaPrincipal {
+    @FXML ImageView virus1;
+    @FXML ImageView virus2;
+    @FXML Image URLvirus;
     public void initialize(){
-        
+        URLvirus = new Image("Imágenes/virus.png");
+        virus1.setImage(URLvirus);
+        virus2.setImage(URLvirus);
     }
 }
